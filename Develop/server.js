66b6ec8508395aa15eeb9aaaa284976a8fb9fbe7
Megan H <megannh6349@gmail.com>
2020-08-06 20:43:34 -0400

    added static to public folder

--- Develop/server.js
+++ Develop/server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 4001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // Create routes
 require("./routes/apiRoutes.js")(app);
